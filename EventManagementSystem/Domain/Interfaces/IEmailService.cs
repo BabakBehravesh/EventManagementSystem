401ad3da39790a495df26367bf12d1d39d54d3ae
Babak Behravesh <babak_behravesh@hotmail.com>
2025-09-29 21:02:50 +0200

    feat: Added EmailService

---
+++ EventManagementSystem/Domain/Interfaces/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿namespace EventManagementSystem.Domain.Interfaces;
+
+public interface IEmailService
+{
+    Task SendWelcomeEmailAsync(string recipientEmail, string recipientName, CancellationToken cancellationToken = default);
+    
+    Task SendPasswordResetEmailAsync(string recipientEmail, string recipientName, string resetLink, CancellationToken cancellationToken = default);
+    
+    Task SendCustomEmailAsync(string recipientEmail, string recipientName, string subject, string htmlContent, string attachmentPath = null, 
+        CancellationToken cancellationToken = default);
+
+    Task SendPasswordChangeConfirmationEmailAsync(string recipientEmail, string recipientName, CancellationToken cancellationToken = default);
+
+    Task SendAccountCreatedEmailAsync(string email, string userName, string password, CancellationToken cancellationToken = default);
+}
+
