04c47e4152964269fc5edbbe8c09f9d92b8661e4
Babak Behravesh <babak_behravesh@hotmail.com>
2025-10-14 19:49:53 +0200

    fix: removing extra references

--- EventManagementSystem/Presentation/Controllers/EventsController.cs
+++ EventManagementSystem/Presentation/Controllers/EventsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
-using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace EventManagementSystem.Controllers;
