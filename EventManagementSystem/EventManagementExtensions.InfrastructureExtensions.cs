a95a61019c2fe00d24565643da5468274c53c6f4
Babak Behravesh <babak.behravesh@gmail.com>
2025-10-27 14:59:31 +0100

    feat: added Azure KeyValut to connect to staging database server

--- EventManagementSystem/EventManagementExtensions.InfrastructureExtensions.cs
+++ EventManagementSystem/EventManagementExtensions.InfrastructureExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace EventManagementSystem;
 
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using EventManagementSystem.Domain.Interfaces;
 using EventManagementSystem.Infrastructure.Email;
 using EventManagementSystem.Infrastructure.QrCode;
@@ -10,13 +12,15 @@ using Serilog;
 
 public static class InfrastructureExtensions
 {
+    private static string _cachedConnectionString = string.Empty;
+
     public static IServiceCollection AddInfrastructureServices(
         this IServiceCollection services,
         IConfiguration configuration)
     {
         // Database
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(GetApplicationConnectionString(configuration)));
 
         // Security - Singletons
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
@@ -31,5 +35,44 @@ public static class InfrastructureExtensions
         Log.Information("Configured Infrastructure Services.");
 
         return services;
+    }
+
+    private static string GetApplicationConnectionString(IConfiguration configuration)
+    {
+
+        if (_cachedConnectionString != null)
+        {
+            return _cachedConnectionString;
+        }
+
+        var keyVaultUrl = configuration["AzureKeyVault:Uri"];
+        var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+        var environmet = (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production").ToLower();
+
+        try
+        {
+            // Get all secrets in parallel to reduce startup time
+            var serverTask = client.GetSecretAsync("sql-server-" + environmet);
+            var databaseTask = client.GetSecretAsync("sql-database-" + environmet);
+            var userIdTask = client.GetSecretAsync("sql-username-" + environmet);
+            var passwordTask = client.GetSecretAsync("sql-password-" + environmet);
+
+            Task.WaitAll(serverTask, databaseTask, userIdTask, passwordTask);
+
+            var server = serverTask.Result.Value.Value;
+            var database = databaseTask.Result.Value.Value;
+            var userId = userIdTask.Result.Value.Value;
+            var password = passwordTask.Result.Value.Value;
+
+            _cachedConnectionString = $"Server={server};Database={database};User Id={userId};Password={password};TrustServerCertificate=True;";
+
+            return _cachedConnectionString;
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Failed to retrieve connection string from Key Vault");
+            throw;
+        }
     }
 }
