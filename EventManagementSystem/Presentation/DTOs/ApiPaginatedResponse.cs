4ae5d94525bd45c12294334cf1a569e8fe4b3284
Babak Behravesh <babak.behravesh@gmail.com>
2025-10-27 20:52:47 +0100

    refactor: removing curly braces for namespace

--- EventManagementSystem/Presentation/DTOs/ApiPaginatedResponse.cs
+++ EventManagementSystem/Presentation/DTOs/ApiPaginatedResponse.cs
@@ -1,39 +1,37 @@
-﻿namespace EventManagementSystem.Presentation.DTOs
+﻿namespace EventManagementSystem.Presentation.DTOs;
+
+public record ApiPaginatedResponse<T>: ApiResponse<IEnumerable<T>>
 {
-    public record ApiPaginatedResponse<T>: ApiResponse<IEnumerable<T>>
+    public int PageNumber { get; init; }
+
+    public int PageSize { get; init; }
+
+    public int TotalCount { get; init; }
+
+    public int TotalPages { get; init; }
+
+    public bool HasPrevious => PageNumber > 1;
+
+    public bool HasNext => PageNumber < TotalPages;
+
+    public static ApiPaginatedResponse<T> SuccessResult(
+        IEnumerable<T> data,
+        int pageNumber,
+        int pageSize, 
+        int totalCount,
+        string message ="Request successful!")
     {
-        public int PageNumber { get; init; }
-
-        public int PageSize { get; init; }
-
-        public int TotalCount { get; init; }
-
-        public int TotalPages { get; init; }
-
-        public bool HasPrevious => PageNumber > 1;
-
-        public bool HasNext => PageNumber < TotalPages;
-
-        public static ApiPaginatedResponse<T> SuccessResult(
-            IEnumerable<T> data,
-            int pageNumber,
-            int pageSize, 
-            int totalCount,
-            string message ="Request successful!")
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+        return new ApiPaginatedResponse<T>
         {
-            var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
-            return new ApiPaginatedResponse<T>
-            {
-                Success = true,
-                Message = message,
-                Data = data,
-                PageNumber = pageNumber,
-                PageSize = pageSize,
-                TotalCount = totalCount,
-                TotalPages = totalPages,
-                StatusCode = StatusCodes.Status200OK
-            };
-        }
+            Success = true,
+            Message = message,
+            Data = data,
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            TotalCount = totalCount,
+            TotalPages = totalPages,
+            StatusCode = StatusCodes.Status200OK
+        };
     }
 }
- 
